b03049003667061391bcd4d44c49242e0f9b7b97
alan.xiao <alan.xiao@d1m.cn>
2022-09-16 11:07:18 +0800

    优化交互

--- index.js
+++ index.js
@@ -2,23 +2,41 @@
 
 const program = require('commander');
 const download = require('download-git-repo');
+const inquirer = require('inquirer')
+// 创建失败提示
+const errorLog = (error) => {
+  console.log(`error -------------------------------------------------------------------${error || 'error'}`)
+}
 //version 版本号
 //name 新项目名称
-program.version('1.0.3', '-v, --version')
-    .command('init <templateName> <projectName>')
-    .action((templateName, projectName) => {
-        if (templateName === "vue") {
-            console.log('clone template ...');
-            download('github:2401345934/webpack-vue-demo', projectName, function (err) {
-                console.log(err ? 'Error' : 'Success')
-            })
-        } else if(templateName === "react") {
+program.version('1.0.4', '-v, --version')
+    .command('create <projectName>')
+    .action((projectName) => {
+      inquirer.prompt(
+        [
+          {
+            type: 'list',
+            name: 'templateType',
+            message: '请选择模版',
+            choices: ['vue', 'react'],
+            default: 'vue',
+          },
+        ],
+      ).then((options) => {
+        const { templateType }  = options
+        if (templateType === "vue") {
+          console.log('clone template ...');
+          download('github:2401345934/webpack-vue-demo', projectName, function (err) {
+              console.log(err ? 'Error' : 'Success')
+          })
+        } else if(templateType === "react") {
             console.log('clone template ...');
             download('github:2401345934/webpack-react-demo', projectName, function (err) {
                 console.log(err ? 'Error' : 'Success')
             })
-        } else {
-          console.error('A template name that does not exist')
         }
+      }).catch((error) => {
+        errorLog(error)
+      })
     });
 program.parse(process.argv);
